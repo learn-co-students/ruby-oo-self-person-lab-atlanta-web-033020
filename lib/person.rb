b9f77669f2a79bf526766b186bf08d7ad2dc3529
github username <github email address>
2020-04-01 11:01:46 -0400

    Pair programming 2

--- lib/person.rb
+++ lib/person.rb
@@ -40,4 +40,27 @@ class Person
         self.hygiene=(@hygiene+4)
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end
+    def work_out
+        self.hygiene=(@hygiene-3)
+        self.happiness=(@happiness+2)
+        "♪ another one bites the dust ♫"
+    end
+    def call_friend(friend)
+        self.happiness=(@happiness+3)
+        friend.happiness=(friend.happiness+3)
+        "Hi #{friend.name}! It's #{@name}. How are you?"
+    end
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness=(@happiness-2)
+            friend.happiness=(friend.happiness-2)
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness=(@happiness+1)
+            friend.happiness=(friend.happiness+1)
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
 end
