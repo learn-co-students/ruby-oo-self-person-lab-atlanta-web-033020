28f4dee69cab50e790f29758823bd1c7e2ccf4db
gustaveH <ghabiman@gmail.com>
2020-04-01 11:07:11 -0400

    tag you're it

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,13 @@
 # your code goes here
+require 'pry'
+class Person 
+    attr_reader :name
+    def initialize (name)
+        @name = name
+    end
+    def 
+    
+end
+new_person = Person.new ("name")
+new_person
+
