c499390dcdd445d6d8e9df3645388465d5b53f19
github username <github email address>
2020-04-01 11:34:03 -0400

    Updating per conventions

--- lib/person.rb
+++ lib/person.rb
@@ -30,42 +30,42 @@ class Person
   end
   
   def clean?
-    @hygiene > 7
+    hygiene > 7
   end
   
   def happy?
-    @happiness > 7
+    happiness > 7
   end
   
-  def get_paid(amount)
-    @bank_account += amount
+  def get_paid(salary)
+    self.bank_account += salary
     "all about the benjamins"
   end
   
   def take_bath
-    self.hygiene = @hygiene + 4
+    self.hygiene = hygiene + 4
     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
   
   def work_out
-    self.happiness = @happiness + 2
-    self.hygiene = @hygiene - 3
+    self.happiness = happiness + 2
+    self.hygiene = hygiene - 3
     "♪ another one bites the dust ♫"
   end
   
   def call_friend(friend)
-    self.happiness = @happiness + 3
+    self.happiness = happiness + 3
     friend.happiness = friend.happiness + 3
-    "Hi #{friend.name}! It's #{@name}. How are you?"
+    "Hi #{friend.name}! It's #{name}. How are you?"
   end
   
   def start_conversation(person, topic)
     if topic == "politics"
-      self.happiness = @happiness - 2
+      self.happiness = happiness - 2
       person.happiness = person.happiness - 2
       "blah blah partisan blah lobbyist"
     elsif topic == "weather"
-      self.happiness = @happiness + 1
+      self.happiness = happiness + 1
       person.happiness = person.happiness + 1
       "blah blah sun blah rain"
     else
