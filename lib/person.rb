07f8cb1b10c0b28dd6d990537cc1967d3baea13e
github username <github email address>
2020-04-02 12:58:16 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,16 @@
 # your code goes here
+require 'pry'
 class Person
     attr_reader :name, :hygiene, :happiness
     attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
     def hygiene=(hygiene)
         if hygiene > 10
             @hygiene = 10
@@ -11,6 +20,7 @@ class Person
             @hygiene = hygiene
         end
     end
+
     def happiness=(happiness)
         if happiness > 10
             @happiness = 10
@@ -20,26 +30,26 @@ class Person
             @happiness = happiness
         end
     end
-    def initialize(name)
-        @name = name
-        @bank_account = 25
-        @happiness = 8
-        @hygiene = 8
-    end
+    
     def happy?
         @happiness > 7
     end
+
     def clean? 
         @hygiene > 7
     end
+
     def get_paid(salary)
-        @bank_account = @bank_account + salary
+        self.bank_account += salary
         "all about the benjamins"
     end
+
     def take_bath
-        self.hygiene=(@hygiene+4)
+        binding.pry
+        self.hygiene=(hygiene+4)
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end
+
     def work_out
         self.hygiene=(@hygiene-3)
         self.happiness=(@happiness+2)
@@ -64,3 +74,5 @@ class Person
         end
     end
 end
+
+binding.pry
